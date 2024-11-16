16e2d72e34668d4fd5c7841475824d288c3f86df
noahpop77 <noahpop77@gmail.com>
2024-11-15 22:53:23 -0800

    Added my Macbooks zshrc config to the script

--- scripts/compileFunction.sh
+++ scripts/compileFunction.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# This will export the following zshrc PS1 terminal settings and get my
+#   terminal looking like how I preffer
+# Looks like:
+#
+# bsawa@Z-Drive src
+# ->  
+#
+export PS1="%{$(tput setaf 36)%}%n%{$(tput setaf 36)%}@%{$(tput setaf 36)%}%m %{$(tput setaf 36)%}%1~
+%{$(tput sgr0)%}-> "
+
 # This function is meant to be added to the end of your ~/.bashrc file
 # so that you can more easily compile your project
 function comp {
