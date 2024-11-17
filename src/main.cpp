fe8f01568f2ffb9bbe75300344f9d3bc7ba31b54
noahpop77 <noahpop77@gmail.com>
2024-11-16 23:01:58 -0800

    Created item, rune (keystones and schools), and summoner mapping files. Added  getItems() to show champ names and the items they purchased to the class. Added itemCodeToName()  for mapping in getItems()

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,33 @@ class match {
                 std::cout << dropFirstAndLast(i["championName"]) << std::endl;
             }
         }
+        
+        // If you want to get the Champion Names and the items they purchased in a game
+        // Utilizes itemCodeToName() to convert the integers in the JSON to strings for
+        //      processing
+        void getItems() {
+            std::cout << "====================" << std::endl;
+            std::cout << "Item Names" << std::endl;
+            std::cout << "--------------------" << std::endl;
+            std::cout << std::endl;
+            
+            for(json i : data["info"]["participants"]){
+                
+                std::cout << dropFirstAndLast(i["championName"]) << std::endl;
+                std::cout << std::endl;
+                std::cout << dropFirstAndLast(itemCodeToName(i["item0"])) << std::endl;
+                std::cout << dropFirstAndLast(itemCodeToName(i["item1"])) << std::endl;
+                std::cout << dropFirstAndLast(itemCodeToName(i["item2"])) << std::endl;
+                std::cout << dropFirstAndLast(itemCodeToName(i["item3"])) << std::endl;
+                std::cout << dropFirstAndLast(itemCodeToName(i["item4"])) << std::endl;
+                std::cout << dropFirstAndLast(itemCodeToName(i["item5"])) << std::endl;
+                std::cout << dropFirstAndLast(itemCodeToName(i["item6"])) << std::endl;
+                std::cout << std::endl;
+                std::cout << "----------" << std::endl;
+            }
+        }
 
+        // Prints the summer name of all players in the lobby
         void getSummonerName() {
             std::cout << "====================" << std::endl;
             std::cout << "Summoner Names" << std::endl;
@@ -45,6 +71,14 @@ class match {
                 return str.substr(0, str.length());
             }
             return ""; // Return an empty string if the input is too short
+        }
+
+        // Maps the integer item codes obtained from the match.json to a string
+        // in the items.json file. Maps item codes to their string names 
+        std::string itemCodeToName(const int itemCode) {
+            std::ifstream f("./mappingFiles/items.json");
+            json items = json::parse(f);
+            return items[std::to_string(itemCode)];
         } 
 };
 
@@ -63,10 +97,13 @@ int main() {
 
     
     // Calls the getChampionNames Instance Member function of class
-    myMatch.getChampNames();
+    //myMatch.getChampNames();
 
     // Calls the getSummonerNames Instance Member function of class
-    myMatch.getSummonerName();
+    //myMatch.getSummonerName();
+
+    // Calls the getItems Instance Member function of the class
+    myMatch.getItems();
 
     return 0;
 }
