b42f3f0d5c7ce9e95ec5d4b87bbb657cb17e177a
noahpop77 <noahpop77@gmail.com>
2024-11-18 00:04:36 -0800

    Finished core of matchBuilder (Random values for almost all values). Still need to determine values and ranges for game time and ID fields. Created champions.json to be able to reference champion data. Cleaned up main.cpp

--- include/matchBuilder.hpp
+++ include/matchBuilder.hpp
@@ -17,29 +17,46 @@ class matchBuilder {
         const std::string templatePath = "../src/mappingFiles/match-template.json";
 
         json randomMatch() {
+            //Imports the match template file that is gonna get copied, transformed, and returned
             std::ifstream file(templatePath);
             json matchTemplate = json::parse(file);
-
+            
+            // TODO: Need to have a proper set of random fields generated for the following
             matchTemplate["info"]["gameCreation"] = "???";
             matchTemplate["info"]["gameDuration"] = "???";
             matchTemplate["info"]["gameEndTimestamp"] = "???";
             matchTemplate["info"]["gameId"] = "???";
             matchTemplate["info"]["gameName"] = "???";
             matchTemplate["info"]["gameStartTimestamp"] = "???";
-            
 
-            for(json participant : matchTemplate["info"]["participants"]){
-                participant["assists"];
-                participant["deaths"];
-                participant["kills"];
+            /*
+            There is a problem that will occur here if you are not careful. If you do not pass the json
+            object to the loop by reference, you will be altering a COPY of the matchTemplate and it will
+            not work as intended.
 
-                participant["goldEarned"];
-                participant["totalDamageDealtToChampions"];
-                participant["champExperience"];
-                participant["champLevel"];
-                participant["championId"];
-                participant["championName"];
+             The original file of match-template.json is unaffected by any changes.
+            */
+            for(json& participant : matchTemplate["info"]["participants"]){
+                
+                // 0-25 is a reasonable amount of kills and assists that 
+                // can reasonably happen in League of Legends
+                participant["assists"] = myRandom::generateRandomInt(0, 25);
+                participant["deaths"] = myRandom::generateRandomInt(0, 25);
+                participant["kills"] = myRandom::generateRandomInt(0, 25);
 
+                // 20k~ gold is around full build
+                participant["goldEarned"] = myRandom::generateRandomInt(1, 20000);
+                // Only time there is more than 90k damage in an average game length wise is if
+                // there is karthus in the game or something 
+                participant["totalDamageDealtToChampions"] = myRandom::generateRandomInt(1, 90000);
+                participant["champExperience"] = myRandom::generateRandomInt(1, 12576);
+                // Level 18 is the cap
+                participant["champLevel"] = myRandom::generateRandomInt(1, 18);
+                participant["championId"] = myRandom::generateRandomInt(1, 200);
+                // Pulls a random champion name from the champions.json file
+                participant["championName"] = getRandomChamp();
+
+                // Random items baby!
                 participant["item0"] = getRandomItemFromJson();
                 participant["item1"] = getRandomItemFromJson();
                 participant["item2"] = getRandomItemFromJson();
@@ -47,43 +64,36 @@ class matchBuilder {
                 participant["item4"] = getRandomItemFromJson();
                 participant["item5"] = getRandomItemFromJson();
                 participant["item6"] = getRandomItemFromJson();
-
+                
+                // Random Summoner Spells baby!
                 participant["summoner1Id"] = getRandomSummonerFromJson();
                 participant["summoner2Id"] = getRandomSummonerFromJson();
-
+                
+                // Random Keystone and secondary school baby!
                 participant["perks"]["styles"][0]["selections"][0]["perk"] = getRandomKeystoneFromJson();
                 participant["perks"]["styles"][1]["style"] = getRandomSecondaryRuneFromJson();
-
+                
+                // Random Summoner Name and Riot Account data baby!
                 participant["riotIdGameName"] = myRandom::generateRandomString(8);
                 participant["riotIdTagline"] = std::string("#") + myRandom::generateRandomString(3);
                 participant["summonerName"] = myRandom::generateRandomString(8);
 
             }
 
+            return matchTemplate;
         }
 
         static std::string getRandomItemFromJson() {
-
             return getRandomFromJson("../src/mappingFiles/items.json");
-            
         }
-
         static std::string getRandomSummonerFromJson() {
-
             return getRandomFromJson("../src/mappingFiles/summoners.json");
-            
         }
-
         static std::string getRandomKeystoneFromJson() {
-
             return getRandomFromJson("../src/mappingFiles/keystones.json");
-
         }
-
         static std::string getRandomSecondaryRuneFromJson() {
-
             return getRandomFromJson("../src/mappingFiles/secondaryRunes.json");
-
         }
 
     
@@ -99,6 +109,32 @@ class matchBuilder {
             return ""; // Return an empty string if the input is too short
         }
 
+        // Based off the champions.json file it will get a random champion name and return it
+        std::string getRandomChamp() {
+            std::ifstream champFile("../src/mappingFiles/champions.json");
+            json championsJson = json::parse(champFile);
+
+            // Stores the names in a vector so that we can search for a random name in a bit
+            std::vector<std::string> champs;
+            for (auto it = championsJson.begin(); it != championsJson.end(); ++it) {
+                champs.push_back(it.key());
+            }
+
+            // Generate a random index
+            std::random_device rd;  // Seed
+            std::mt19937 gen(rd()); // Random number generator
+            std::uniform_int_distribution<> distrib(0, champs.size() - 1);
+
+            int randomIndex = distrib(gen);
+
+            // Return the random key
+            return dropFirstAndLast(champs[randomIndex]);
+
+        }
+
+        // Gets a random key from a JSON file thats passed in. This is flexible 
+        // for items, summoners, keystone runes, and secondary runes
+        // They need to be IDs for use in MTRACK
         static std::string getRandomFromJson(const std::string& filename) {
             // Open and parse the JSON file
             std::ifstream file(filename);
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,18 @@
 #include <iostream>
-#include "../include/match.hpp"
 #include "../include/matchBuilder.hpp"
 #include "../include/json.hpp"
-#include "../include/apiClient.hpp"
+// #include "../include/apiClient.hpp"
 
 using json = nlohmann::json;
 
 int main() {
 
     // Creates a readable JSON object based off the match.json file passed to it
-    std::ifstream f("match.json");
-    json data = json::parse(f);
+    // std::ifstream f("match.json");
+    // json data = json::parse(f);
 
-    // Declares the myMatch variable of type match which is our class
-    // Defines the matches public data variable with the JSON object we created
-
-    /*
-    match myMatch;
-    myMatch.data = data;
-    myMatch.getRunes();
-    myMatch.getChampNames();
-    myMatch.getSummonerName();
-    myMatch.getItems();
-    myMatch.getSummoners();
-    */
-
-    std::cout << "Random Item Code: " << matchBuilder::getRandomItemFromJson() << std::endl;
-    std::cout << "Random Item Code: " << matchBuilder::getRandomItemFromJson() << std::endl;
-    std::cout << "Random Item Code: " << matchBuilder::getRandomItemFromJson() << std::endl;
-    std::cout << "Random Item Code: " << matchBuilder::getRandomItemFromJson() << std::endl;
-    std::cout << "Random Item Code: " << matchBuilder::getRandomItemFromJson() << std::endl;
-
-    std::cout << "Random Summoner Code: " << matchBuilder::getRandomSummonerFromJson() << std::endl;
-    std::cout << "Random Summoner Code: " << matchBuilder::getRandomSummonerFromJson() << std::endl;
-    std::cout << "Random Summoner Code: " << matchBuilder::getRandomSummonerFromJson() << std::endl;
-    std::cout << "Random Summoner Code: " << matchBuilder::getRandomSummonerFromJson() << std::endl;
-    std::cout << "Random Summoner Code: " << matchBuilder::getRandomSummonerFromJson() << std::endl;
-
-    std::cout << "Random Keystone Code: " << matchBuilder::getRandomKeystoneFromJson() << std::endl;
-    std::cout << "Random Keystone Code: " << matchBuilder::getRandomKeystoneFromJson() << std::endl;
-    std::cout << "Random Keystone Code: " << matchBuilder::getRandomKeystoneFromJson() << std::endl;
-    std::cout << "Random Keystone Code: " << matchBuilder::getRandomKeystoneFromJson() << std::endl;
-    std::cout << "Random Keystone Code: " << matchBuilder::getRandomKeystoneFromJson() << std::endl;
-
-    std::cout << "Random Secondary Runes Code: " << matchBuilder::getRandomSecondaryRuneFromJson() << std::endl;
-    std::cout << "Random Secondary Runes Code: " << matchBuilder::getRandomSecondaryRuneFromJson() << std::endl;
-    std::cout << "Random Secondary Runes Code: " << matchBuilder::getRandomSecondaryRuneFromJson() << std::endl;
-    std::cout << "Random Secondary Runes Code: " << matchBuilder::getRandomSecondaryRuneFromJson() << std::endl;
-    std::cout << "Random Secondary Runes Code: " << matchBuilder::getRandomSecondaryRuneFromJson() << std::endl;
+    matchBuilder randMatch;
+    std::cout << randMatch.randomMatch().dump(4) << std::endl;
 
     return 0;
 }
