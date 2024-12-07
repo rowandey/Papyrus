12c55503247e9f455692e87bb23d13a118816528
noahpop77 <noahpop77@gmail.com>
2024-12-06 16:28:06 -0800

    Functional OpenSSL Module compiled with Papy. SSL GET is working, SSL POST needs testing.

--- src/apiClient.hpp
+++ src/apiClient.hpp
@@ -1,40 +1,33 @@
 #pragma once
-
-#include "httplib.h"
+#include <string>
+#include <memory>
 #include "json.hpp"
+#include <variant>
+
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+
 
 using json = nlohmann::json;
 
 class ApiClient {
 public:
-    // Constructor to initialize the server address
-    ApiClient(const std::string& serverAddress);
+    explicit ApiClient(const std::string& serverAddress);
 
-    // Set the API endpoint
     void setEndpoint(const std::string& endpoint);
-
-    // Set the API parameter
     void setParameter(const std::string& parameter);
+    void setPayload(const nlohmann::json& payload);
 
-    // Set the JSON payload
-    void setPayload(const json& payload);
-
-    // Send GET request
     std::string sendGETRequest();
-
-    // TODO: Method for DELETE request
-    // TODO: Method for PUT request
-
-    // Send POST request
     std::string sendPOSTRequest();
 
 private:
-    httplib::Client client; // HTTP client
-    std::string serverAddress; // Server address
-    std::string endpoint; // API endpoint
-    std::string parameter; // API parameters
-    json payload; // JSON payload
+    std::unique_ptr<httplib::Client> client;
+    std::unique_ptr<httplib::SSLClient> sslClient;
 
-    // Error handling function
+    std::string endpoint;
+    std::string parameter;
+    nlohmann::json payload;
+
     std::string errorToString(httplib::Error err);
 };
